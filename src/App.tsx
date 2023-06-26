e614fe5d94a9900e9a61f62443be6585067ea399
VasylMolnar <vasylmolnar281@gmail.com>
2023-06-26 17:28:42 +0300

    new

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,12 @@ const App: FC = () => {
       <Route path="/" element={<Header />}>
         <Route path="/">
           <Route index element={<UsersPage />} />
-          <Route path=":id" element={<h1>UsersPage:ID</h1>} />
+          {/* <Route path=":id" element={<h1>UsersPage:ID</h1>} /> */}
         </Route>
 
         <Route path="todos">
           <Route index element={<TodosPage />} />
-          <Route path=":id" element={<h1>todos:ID</h1>} />
+          {/* <Route path=":id" element={<h1>todos:ID</h1>} /> */}
         </Route>
       </Route>
     </Routes>
--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/TypeScript_React">
       <Routes>
         <Route path="/*" element={<App />} />
       </Routes>
